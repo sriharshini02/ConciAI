617f69ad6088a70bb6485632e6ec787e7267c7a7
sriharshini02 <sriharshinibhupathiraju@gmail.com>
2025-07-20 16:31:34 +0530

    - resolved attribute error

--- main/forms.py
+++ main/forms.py
@@ -5,11 +5,10 @@ from .models import GuestRoomAssignment, Room, Amenity # Import Amenity model
 from django.core.exceptions import ValidationError
 from django.utils import timezone
 class GuestRoomAssignmentForm(forms.ModelForm):
-    # Change room_number to a CharField for text input
     room_number_input = forms.CharField(
-        max_length=50, 
+        max_length=50,
         required=True,
-        label="Room Number", # Custom label for clarity in the form
+        label="Room Number",
         widget=forms.TextInput(attrs={'placeholder': 'e.g., 101, 205A'})
     )
 
@@ -22,28 +21,32 @@ class GuestRoomAssignmentForm(forms.ModelForm):
         model = GuestRoomAssignment
         fields = [
             'guest_names',
-            # Removed 'num_adults', 'num_children', 'notes' as they are not in GuestRoomAssignment model
-            'status', 'total_bill_amount'
-            # 'check_in_time' and 'check_out_time' are handled by clean method
+            'status',
+            'total_bill_amount',
+            # Note: check_in_time and check_out_time are handled by clean method
+            # and should not be in 'fields' if you want to use check_in_date/time_input
         ]
-        # If you have num_adults, num_children, or notes in GuestRoomAssignment model, add them back here.
-        # Based on your models.py, GuestRoomAssignment has:
-        # room_number (ForeignKey to Room)
-        # guest_names (CharField)
-        # check_in_time (DateTimeField)
-        # check_out_time (DateTimeField)
-        # status (CharField)
-        # total_bill_amount (DecimalField)
-        # created_at (DateTimeField)
-        # updated_at (DateTimeField)
-        # hotel (ForeignKey to Hotel)
+        # Define the order of fields explicitly
+        # Include 'room_number_input' here, and any other fields from 'fields' list
+        field_order = [
+            'room_number_input', # This will make it appear first
+            'guest_names',
+            'check_in_date',
+            'check_in_time_input',
+            'check_out_date',
+            'check_out_time_input',
+            'status',
+            'total_bill_amount',
+            # Add any other fields from GuestRoomAssignment model that you want to display
+        ]
+
 
     def __init__(self, *args, **kwargs):
         self.hotel = kwargs.pop('hotel', None)
         super().__init__(*args, **kwargs)
         
-        # Add room_number_input to the form's fields order
-        self.fields.insert(0, 'room_number_input', self.fields.pop('room_number_input'))
+        # Remove the problematic line:
+        # self.fields.insert(0, 'room_number_input', self.fields.pop('room_number_input'))
 
         # Set initial values for date/time fields if instance exists
         if self.instance.pk:
@@ -51,34 +54,28 @@ class GuestRoomAssignmentForm(forms.ModelForm):
             self.fields['check_in_time_input'].initial = self.instance.check_in_time.time()
             self.fields['check_out_date'].initial = self.instance.check_out_time.date()
             self.fields['check_out_time_input'].initial = self.instance.check_out_time.time()
-            # If editing an existing assignment, populate room_number_input
-            if self.instance.room_number: # Check if room_number is not None
+            if self.instance.room_number:
                 self.fields['room_number_input'].initial = self.instance.room_number.room_number
 
 
     def clean(self):
         cleaned_data = super().clean()
         
-        # Handle room_number_input: find or create Room object
         room_number_str = cleaned_data.get('room_number_input')
         if room_number_str and self.hotel:
-            # Try to get the room, or create it if it doesn't exist
             room_obj, created = Room.objects.get_or_create(
                 hotel=self.hotel,
                 room_number=room_number_str,
-                defaults={'status': 'available'} # Default status for new rooms
+                defaults={'status': 'available'}
             )
-            # Assign the Room object to the instance's room_number field
-            self.instance.room_number = room_obj 
+            self.instance.room_number = room_obj
             if created:
-                # This message will appear in Django's form errors but won't stop submission
                 self.add_error(None, f"Room '{room_number_str}' created and assigned.")
         elif not self.hotel:
             self.add_error(None, "Cannot assign room without a hotel context.")
         else:
             self.add_error('room_number_input', "Room number is required.")
 
-        # Combine date and time inputs into datetime objects
         check_in_date = cleaned_data.get('check_in_date')
         check_in_time_input = cleaned_data.get('check_in_time_input')
         check_out_date = cleaned_data.get('check_out_date')
@@ -88,7 +85,7 @@ class GuestRoomAssignmentForm(forms.ModelForm):
             cleaned_data['check_in_time'] = timezone.make_aware(
                 timezone.datetime.combine(check_in_date, check_in_time_input)
             )
-        else: # Ensure these fields are always set or raise an error
+        else:
             self.add_error('check_in_date', 'Check-in date is required.')
             self.add_error('check_in_time_input', 'Check-in time is required.')
 
@@ -96,7 +93,7 @@ class GuestRoomAssignmentForm(forms.ModelForm):
             cleaned_data['check_out_time'] = timezone.make_aware(
                 timezone.datetime.combine(check_out_date, check_out_time_input)
             )
-        else: # Ensure these fields are always set or raise an error
+        else:
             self.add_error('check_out_date', 'Check-out date is required.')
             self.add_error('check_out_time_input', 'Check-out time is required.')
         
@@ -105,15 +102,13 @@ class GuestRoomAssignmentForm(forms.ModelForm):
     def save(self, commit=True):
         instance = super().save(commit=False)
         
-        # The room_number_input has already been processed in clean()
-        # and self.instance.room_number should now hold the correct Room object.
-        
         if self.hotel and not instance.hotel:
             instance.hotel = self.hotel
         
         if commit:
             instance.save()
         return instance
+
 
 
 
